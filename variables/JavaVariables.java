5788dd68de0029036fc564f589bb93af5c22e18a
polvo ron <proceso_distribuido@yahoo.com.mx>
2023-02-06 11:33:47 -0600

    initial version

---
+++ variables/JavaVariables.java
@@ -0,0 +1,16 @@
+public class JavaVariables{
+    public static void main(String[] args){
+		int _a;
+		int $a,
+		    _____2_w,
+			_$;
+			
+		
+//		_a = 0;
+		$a = 25;
+		_____2_w = 30;
+			_$   = -4;
+        System.out.println("The value of the variables are: " + _a + " and " 
+		+ $a + " , " + _____2_w + " , " + _$ );
+    }// End of main
+}// End of JavaVariables Class
